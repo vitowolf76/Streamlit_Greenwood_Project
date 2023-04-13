02897e878a8b8828e01a7e9128eaab3064e2d299
vitowolf <vitowolf76@gmail.com>
2023-04-13 16:20:37 -0600

    deleted import

--- Slider_Alpha.py
+++ Slider_Alpha.py
@@ -1,15 +1,8 @@
 
-
-
-###############test run 4 now add the values as compared to Male, Female, Non Gender, age and student status
-
-
-
 import streamlit as st
 import pandas as pd
 import altair as alt
 import datetime as dt
-import matplotlib.pyplot as plt
 
 
 ALL_DATA_URL = "./slider_bar_alpha.csv"
@@ -31,10 +24,6 @@ all_data["Date_of_Birth"] = pd.to_datetime(all_data["Date_of_Birth"], errors="co
 
 # Calculate the age based on the date of birth
 all_data["Age"] = (dt.datetime.now() - all_data["Date_of_Birth"]) // dt.timedelta(days=365.2425)
-
-# Create a slider for selecting the age range
-
-
 
 # Filter the data based on the selected age range
 min_age = int(all_data["Age"].min())
@@ -65,24 +54,10 @@ st.write(f"Student Status Counts (age {age_range[0]}-{age_range[1]})")
 st.altair_chart(chart, use_container_width=True)
 
 # define the ordinal values you want to analyze
-
-def load_all_data(nrows):
-    data = pd.read_csv(ALL_DATA_URL, nrows=nrows)
-
-    def load_all_data(nrows):
-        data = pd.read_csv(ALL_DATA_URL, nrows=nrows)
-        return data
-
-    all_data = load_all_data(100000)
-
-
 ordinal_values = ['Essential', 'Very Important', 'Desirable', 'Not Important']
 data = all_data.replace("Very important", "Very Important")
 # define the columns you want to analyze
 columns_to_analyze = ['Appearance', 'Assertiveness', 'Being_Someone_Special', 'Career', 'Control', 'Emotional_Stability', 'Family', 'Financial_Security', 'Friendships', 'Health', 'Independence', 'Intelligence', 'Love', 'Marriage', 'Personal_Recognition', 'Relationships', 'Spiritual_Life', 'Status', 'Success', 'Able_to_Make_a_Difference', 'Able_to_See_Results', 'Be_Own_Boss', 'Business_Location', 'Challenge', 'Competition_on_the_Job', 'Contribute_to_Society', 'Creativity', 'Excitement', 'Fast_Paced', 'Flexibility', 'Good_Benefits', 'Intellectual_Growth', 'Job_Security', 'Leadership', 'Manage_People', 'Outdoors', 'Personal_Leisure_Time', 'Power', 'Prestige', 'Promotion', 'Regular_Hours', 'Respect', 'Teamwork', 'Time_Alone', 'Time_for_Family', 'To_Be_Known', 'Travel', 'Variety', 'Wealth', 'Helping_People']
-# all_data_no_missing = all_data.dropna(subset=['Appearance'])
-# st.write(all_data_no_missing)
-# iterate over the columns and ordinal values, and calculate the percentages
 percentages = {}
 for column in columns_to_analyze:
     column_name = column.replace("_"," ")
