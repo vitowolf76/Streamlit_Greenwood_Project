87b36e02c4282be965cf4cefec780a1c5d778aad
vitowolf <vitowolf76@gmail.com>
2023-04-13 16:28:12 -0600

    cleaned extra data

--- main.py
+++ main.py
@@ -1,104 +1,78 @@
+
 import streamlit as st
-import numpy as np
 import pandas as pd
-from dateutil.parser import parse
-import datetime as dt
-import matplotlib.pyplot as plt
 import altair as alt
-import plotly.graph_objs as go
-
-# objective = Figure out how times each of the career values equals  essential , Very important , desirable and not import
-# create an empty dictionary to store the percentages ( also can we creat a slider to show what age the student is and want they find important) exploration of values based on age and gender need to rework age  Y2K problem
-# # # Display the dataframe as a table using Stream
-
-DATA_URL_1 = ("/Users/vitowolf/PycharmProject/Streamlit_Greenwood_Project_2/all_data.csv")
-DATA_URL_2= ("/Users/vitowolf/PycharmProject/Streamlit_Greenwood_Project_2/all_values .csv")
-DATA_URL_3= ("/Users/vitowolf/PycharmProject/Streamlit_Greenwood_Project_2/little_value.csv")
-#I added two csv files and I am using little_value/data3 to experiment with cleaning column names for eassier plotting
-
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL_1, nrows=nrows)
-    data2 = pd.read_csv(DATA_URL_2, nrows=nrows)
-    data3 = pd.read_csv(DATA_URL_3, nrows=nrows)
-    combined_data = pd.concat([data, data2, data3], axis=0)
-    return combined_data
-
-# this section of code was the original load to read all_data.csv
-# def load_data(nrows):
-#     data = pd.read_csv(DATA_URL_1)
-#     return data
+import datetime as dt
 
 
+ALL_DATA_URL = "./slider_bar_alpha.csv"
+
+def load_all_data(nrows):
+    data = pd.read_csv(ALL_DATA_URL, nrows=nrows)
+    return data
+
+all_data = load_all_data(100000)
 st.markdown(
     f'<div style="text-align:center"><h1>GREENWOOD PROJECT</h1></div>',
-    unsafe_allow_html=True
+    unsafe_allow_html=True)
+
+st.markdown(
+    f'<div style="text-align:center"><h1></h1></div>',
+    unsafe_allow_html=True)
+# Convert the date of birth to a datetime object
+all_data["Date_of_Birth"] = pd.to_datetime(all_data["Date_of_Birth"], errors="coerce")
+
+# Calculate the age based on the date of birth
+all_data["Age"] = (dt.datetime.now() - all_data["Date_of_Birth"]) // dt.timedelta(days=365.2425)
+
+# Filter the data based on the selected age range
+min_age = int(all_data["Age"].min())
+max_age = int(all_data["Age"].max())
+age_range = st.slider("Select an age range", min_age, max_age, (min_age, max_age),key=f"first_slider")
+filtered_data = all_data[all_data["Age"].between(age_range[0], age_range[1])]
+
+# Filter data based on selected gender and age range
+gender = st.selectbox("Select a gender", ["Male", "Female", "Gender non-conforming"])
+filtered_data = filtered_data[filtered_data["To_which_gender_do_you_identify?"] == gender]
+
+
+filtered_data = filtered_data[
+    (filtered_data["Age"] >= age_range[0]) & (filtered_data["Age"] <= age_range[1])
+]
+
+# Compute value counts of "Are you currently a student?" for the filtered data
+status_counts = filtered_data["Are_you_currently_a_student?"].value_counts().reset_index()
+status_counts.columns = ["status", "count"]
+
+# Create a pie chart of the filtered data
+chart = alt.Chart(status_counts).mark_bar().encode(
+    x="status",
+    y="count",
+    color="status"
 )
-# The code block below is to add bar chart for all_data.csv only
+st.write(f"Student Status Counts (age {age_range[0]}-{age_range[1]})")
+st.altair_chart(chart, use_container_width=True)
 
-# data = load_data(3000)
-# print(data)
-# st.write(data)
-# # this section is creating the first charts using all_data.csv info
-# x = data["Are_you_currently_a_student?"]
-# st.write(x)
-# y = data["State/Region"]
-# st.write(y)
-# z = data["City"]
-# st.write(z)
-# d = data["Date_of_Birth"]
-# st.write(d)
-#
-# city = z.value_counts()
-# st.bar_chart(city)
-#
-# counts = x.value_counts()
-# st.bar_chart(counts)
-#
-# states = y.value_counts()
-# st.bar_chart(states)
-#
-# d = pd.to_datetime(d, errors='coerce')
-# age = (dt.datetime.now() - d) // dt.timedelta(days=365.2425)
-# st.write(age)
-#
-# age = age.value_counts()
-# st.bar_chart(age)
-#
-# data = load_data(3000)
-# print(data)
-# st.write(data)
-
-age_range = st.slider('Select age range:', 10, 90, (20, 50))
-gender = st.selectbox('Select gender:', ['Male', 'Female', 'Other'])
-
-# Extract the data for the "Essential", "Very Important", "Desirable", and "Not Important" categories def load_data(nrows):
-
-
-data = load_data(3000)
-data3 = pd.read_csv(DATA_URL_3)
-
-
-
-st.write(data3)
-
-
-
-# # define the ordinal values you want to analyze
+# define the ordinal values you want to analyze
 ordinal_values = ['Essential', 'Very Important', 'Desirable', 'Not Important']
-data3 = data3.replace("Very important", "Very Important")
+data = all_data.replace("Very important", "Very Important")
 # define the columns you want to analyze
-columns_to_analyze = ['Appearance', 'Assertiveness', 'Being_Someone_Special', 'Career', 'Control', 'Emotional_Stability', 'Family', 'Financial_Security', 'Friendships', 'Health', 'Independence', 'Intelligence', 'Love', 'Marriage', 'Personal_Recognition', 'Relationships', 'Spiritual_Life', 'Status', 'Success', 'Able_to_Make_a_Difference', 'Able_to_See_Results', 'Be_Own_Boss', 'Business_Location', 'Challenge', 'Competition_on_the_Job', 'Contribute_to_Society', 'Creativity', 'Excitement', 'Fast_Paced', 'Flexibility', 'Good_Benefits', 'Independence', 'Intellectual_Growth', 'Job_Security', 'Leadership', 'Manage_People', 'Outdoors', 'Personal_Leisure_Time', 'Power', 'Prestige', 'Promotion', 'Regular_Hours', 'Respect', 'Teamwork', 'Time_Alone', 'Time_for_Family', 'To_Be_Known', 'Travel', 'Variety', 'Wealth', 'Helping_People']
-data3_no_missing = data3.dropna(subset=['Appearance'])
-st.write(data3_no_missing)
-# iterate over the columns and ordinal values, and calculate the percentages
+columns_to_analyze = ['Appearance', 'Assertiveness', 'Being_Someone_Special', 'Career', 'Control', 'Emotional_Stability', 'Family', 'Financial_Security', 'Friendships', 'Health', 'Independence', 'Intelligence', 'Love', 'Marriage', 'Personal_Recognition', 'Relationships', 'Spiritual_Life', 'Status', 'Success', 'Able_to_Make_a_Difference', 'Able_to_See_Results', 'Be_Own_Boss', 'Business_Location', 'Challenge', 'Competition_on_the_Job', 'Contribute_to_Society', 'Creativity', 'Excitement', 'Fast_Paced', 'Flexibility', 'Good_Benefits', 'Intellectual_Growth', 'Job_Security', 'Leadership', 'Manage_People', 'Outdoors', 'Personal_Leisure_Time', 'Power', 'Prestige', 'Promotion', 'Regular_Hours', 'Respect', 'Teamwork', 'Time_Alone', 'Time_for_Family', 'To_Be_Known', 'Travel', 'Variety', 'Wealth', 'Helping_People']
 percentages = {}
 for column in columns_to_analyze:
-    st.markdown(f'<div style="text-align:center"><h1>{column}</h1></div>', unsafe_allow_html=True)
+    column_name = column.replace("_"," ")
+    st.markdown(f'<div style="text-align:center"><h1>{column_name}</h1></div>', unsafe_allow_html=True)
     percentages[column] = {}
+    gender = st.selectbox("Select a gender", ["Male", "Female", "Gender non-conforming"], key=f"gender_checkbox_{column}")
+    min_age = int(all_data["Age"].min())
+    max_age = int(all_data["Age"].max())
+    age_range = st.slider("Select an age range", min_age, max_age, (min_age, max_age),key=f"slider_age_{column}")
     for ordinal_value in ordinal_values:
-        value_counts = data3_no_missing[column].value_counts()
+        filtered_data = data[data["Age"].between(age_range[0], age_range[1])]
+        filtered_data = filtered_data[filtered_data["To_which_gender_do_you_identify?"] == gender]
+        value_counts = filtered_data[column].value_counts()
         ordinal_frequency = value_counts[ordinal_value] if ordinal_value in value_counts else 0
-        ordinal_percentage = (ordinal_frequency / len(data3_no_missing)) * 100
+        ordinal_percentage = (ordinal_frequency / len(filtered_data)) * 100
         percentages[column][ordinal_value] = ordinal_percentage
         st.write(f"The percentage of {ordinal_value} is: {ordinal_percentage:.2f}%")
     st.bar_chart(percentages[column])
@@ -108,37 +82,3 @@ for column in columns_to_analyze:
 
 
 
-    # Create a function to plot the pie chart
-    def plot_pie_chart(percentages, column):
-        fig, ax = plt.subplots()
-        ax.pie(percentages, labels=percentages.keys(), colors=[colors[k] for k in percentages.keys()],
-               autopct='%1.1f%%', startangle=90)
-        ax.axis('equal')
-        ax.set_title(column)
-        return fig
-
-    fig = go.Figure(data=[go.Pie(labels=ordinal_values, values=list(percentages[column].values()))])
-    st.plotly_chart(fig)
-
-#
-#
-
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
