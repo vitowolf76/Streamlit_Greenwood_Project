b3aeac0ec40ea774ff5c7a425bd4f5c1e15d4303
vitowolf <vitowolf76@gmail.com>
2023-04-14 13:37:37 -0600

    adding header explanation

--- main.py
+++ main.py
@@ -15,10 +15,13 @@ all_data = load_all_data(100000)
 st.markdown(
     f'<div style="text-align:center"><h1>GREENWOOD PROJECT</h1></div>',
     unsafe_allow_html=True)
-
+st.markdown(
+    f'<div style="text-align:center"><h3>This is a data visualization exploration of the GS120 values N=2006. Use the Age slider and/or Gender toggle to specify population for visualization</h3></div>',
+    unsafe_allow_html=True)
 st.markdown(
     f'<div style="text-align:center"><h1></h1></div>',
     unsafe_allow_html=True)
+
 # Convert the date of birth to a datetime object
 all_data["Date_of_Birth"] = pd.to_datetime(all_data["Date_of_Birth"], errors="coerce")
 
@@ -44,7 +47,7 @@ filtered_data = filtered_data[
 status_counts = filtered_data["Are_you_currently_a_student?"].value_counts().reset_index()
 status_counts.columns = ["status", "count"]
 
-# Create a pie chart of the filtered data
+# Create a bar chart of the filtered data of student status
 chart = alt.Chart(status_counts).mark_bar().encode(
     x="status",
     y="count",
@@ -53,10 +56,11 @@ chart = alt.Chart(status_counts).mark_bar().encode(
 st.write(f"Student Status Counts (age {age_range[0]}-{age_range[1]})")
 st.altair_chart(chart, use_container_width=True)
 
-# define the ordinal values you want to analyze
+# define the ordinal values you want to analyze from GS120 values
 ordinal_values = ['Essential', 'Very Important', 'Desirable', 'Not Important']
 data = all_data.replace("Very important", "Very Important")
-# define the columns you want to analyze
+
+# define the columns you want to analyze from GS120 values (with slider bar for age and Gender toggle)
 columns_to_analyze = ['Appearance', 'Assertiveness', 'Being_Someone_Special', 'Career', 'Control', 'Emotional_Stability', 'Family', 'Financial_Security', 'Friendships', 'Health', 'Independence', 'Intelligence', 'Love', 'Marriage', 'Personal_Recognition', 'Relationships', 'Spiritual_Life', 'Status', 'Success', 'Able_to_Make_a_Difference', 'Able_to_See_Results', 'Be_Own_Boss', 'Business_Location', 'Challenge', 'Competition_on_the_Job', 'Contribute_to_Society', 'Creativity', 'Excitement', 'Fast_Paced', 'Flexibility', 'Good_Benefits', 'Intellectual_Growth', 'Job_Security', 'Leadership', 'Manage_People', 'Outdoors', 'Personal_Leisure_Time', 'Power', 'Prestige', 'Promotion', 'Regular_Hours', 'Respect', 'Teamwork', 'Time_Alone', 'Time_for_Family', 'To_Be_Known', 'Travel', 'Variety', 'Wealth', 'Helping_People']
 percentages = {}
 for column in columns_to_analyze:
@@ -76,8 +80,6 @@ for column in columns_to_analyze:
         percentages[column][ordinal_value] = ordinal_percentage
         st.write(f"The percentage of {ordinal_value} is: {ordinal_percentage:.2f}%")
     st.bar_chart(percentages[column])
-
-
 
 
 
